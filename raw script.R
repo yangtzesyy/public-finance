4b8e6d4598cb538d09f46b052d0be78a97231ba0
Yangtzesyy <yangtzesyy@gmail.com>
2020-10-15 20:22:21 +0800

    (May-Mar)/Mar, p=0.02

--- raw script.R
+++ raw script.R
@@ -1,18 +1,46 @@
 ## packages
-if(!require(pacman)); install.packages("pacman")
-library(pacman)
-p_load(tidyverse)
+if(!require(pacman))install.packages("pacman"); library(pacman)
+p_load(tidyverse,readxl)
 
 ##data import
-large_sample <- read_excel("large_sample.xlsx")
-names(large_sample)<-c("RealEstateName","January","February","March","April","May","June",
-                       "July","August","September","October","November","December","SchoolHouse")
-large_sample<-within(large_sample,{
+small_sample <- read_excel("small-sample.xlsx")
+names(small_sample)<-c("RealEstateName","19-Oct","19-Nov","19-Dec","20-Jan","20-Feb","20-Mar",
+                       "20-Apr","20-May","20-Jun","20-Jul","20-Aug","20-Sep","SchoolHouse")
+small_sample<-within(small_sample,{
   SchoolHouse<-as.factor(SchoolHouse)
 })
+
 ## trial
 
-test<-head(large_sample)
-
-test<-lm(April~SchoolHouse,data = large_sample)
+small_sample_AprilMinusMarch<-small_sample%>%
+  mutate(AprilMinusMarch=`20-Apr`-`20-Mar`)#四月减三月，绝对量无关
+test<-lm(AprilMinusMarch~SchoolHouse,data = small_sample_AprilMinusMarch)
 summary(test)
+
+small_sample_AprilMinusMarch<-small_sample%>%
+  mutate(AprilMinusMarch=(`20-Apr`-`20-Mar`)/`20-Mar`)#四月减三月，增速都无关
+test<-lm(AprilMinusMarch~SchoolHouse,data = small_sample_AprilMinusMarch)
+summary(test)
+
+
+small_sample_MayMinusMarch<-small_sample%>%
+  mutate(MayMinusMarch=`20-May`-`20-Mar`)#五月减三月，绝对量无关
+test<-lm(MayMinusMarch~SchoolHouse,data = small_sample_MayMinusMarch)
+summary(test)
+
+small_sample_MayMinusMarch<-small_sample%>%
+  mutate(MayMinusMarch=(`20-May`-`20-Mar`)/`20-Mar`)#五月减三月，增速相关
+test<-lm(MayMinusMarch~SchoolHouse,data = small_sample_MayMinusMarch)
+summary(test)
+
+
+small_sample_MayMinusApril<-small_sample%>%
+  mutate(MayMinusApril=`20-May`-`20-Apr`)#五月减四月，绝对量无关
+test<-lm(MayMinusApril~SchoolHouse,data = small_sample_MayMinusApril)
+summary(test)
+
+small_sample_MayMinusApril<-small_sample%>%
+  mutate(MayMinusApril=(`20-May`-`20-Apr`)/`20-Apr`)#五月减四月，增速无关
+test<-lm(MayMinusApril~SchoolHouse,data = small_sample_MayMinusApril)
+summary(test)
+  
